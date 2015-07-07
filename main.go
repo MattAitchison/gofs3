3a25b99ad9eadc484b031ef7e2bc3a9734e1cd6b
Matt Aitchison <Matt@lanciv.com>
2015-07-07 17:51:21 -0500

    File watcher working, s3 having issues.

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"gopkg.in/fsnotify.v1"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatal("wrong number of arguments")
+	}
+	src, _ := os.Args[1], os.Args[2]
+
+	creds := credentials.NewEnvCredentials()
+	if _, err := creds.Get(); err != nil {
+		log.Fatal(err)
+	}
+
+	svc := s3.New(&aws.Config{
+		Credentials:      creds,
+		Region:           "us-east-2",
+		Endpoint:         "s3.amazonaws.com",
+		S3ForcePathStyle: true,
+	})
+
+	uploader := s3manager.NewUploader(&s3manager.UploadOptions{S3: svc})
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				log.Println("event:", event)
+				switch event.Op {
+				case fsnotify.Create:
+					handleCreate(watcher, uploader, event.Name)
+				case fsnotify.Write:
+					log.Println("modified file:", event.Name)
+				}
+
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			log.Println(path)
+			return watcher.Add(path)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	<-done
+
+}
+
+func handleCreate(watcher *fsnotify.Watcher, u *s3manager.Uploader, path string) error {
+	fi, _ := os.Lstat(path)
+
+	if fi.IsDir() {
+		return watcher.Add(path)
+	}
+
+	r, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	name := fi.Name()
+	_, err = u.Upload(&s3manager.UploadInput{
+		Bucket: &os.Args[2],
+		Body:   r,
+		Key:    &name,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
